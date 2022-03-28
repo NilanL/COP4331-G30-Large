f6680d0f4d054312b52d9858cef79db6da5b322d
Shayne <shaynet0809@gmail.com>
2022-03-28 13:06:21 -0400

    everything for email

---
+++ verifylocal.js
@@ -0,0 +1,25 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
+
+function betweenRandomNumber(min, max) {  
+    return Math.floor(
+      Math.random() * (max - min + 1) + min
+    )
+};
+
+const code = betweenRandomNumber(10000, 99999);
+
+const msg = {
+    to: 'shaynet0809@gmail.com',
+    from: 'dailygrind4331@gmail.com',
+    subject: 'Testing 5 digit code',
+    text: `Your 5 digit code is: ${code}`,
+};
+
+sgMail.send(msg, function(err, result){
+    if(err) {
+        console.log('Email not sent error occurred.');
+    } else {
+        console.log('Email was sent!');
+    }
+});
