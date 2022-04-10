0077369ad94a1519376622f7ccd872c9d8d5d580
Mari Peele <mpeele@geometricprogress.com>
2022-04-09 22:19:22 -0400

    updated service js

--- server.js
+++ server.js
@@ -1,81 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const sendEmail = require('./sendEmail');
 
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', PORT);
 app.use(cors());
 app.use(bodyParser.json());
 
-require('dotenv').config();
 const url = process.env.MONGODB_URI;
+const url2 = process.env.MONGODB_HEALTHDB_URI;
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
+const client2 = new MONGO_CLIENT_EVENTS(url2);
 client.connect();
+client2.connect();
 
+let api = require('./api.js');
+let api2 = require('./habitsApi.js');
+const { MONGO_CLIENT_EVENTS } = require('mongodb');
+api.setApp( app, client);
+api2.setApp(app, client2);
 
 ///////////////////////////////////////////////////
 // For Heroku deployment
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') 
-{
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('frontend/build'));
-  app.get('*', (req, res) => 
-  {
+  app.get('/*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
-
-app.post('/api/login', async (req, res, next) => {
-    var error = '';
-
-    const { username, password } = req.body;
-
-    const db = client.db();
-    const results = await db.collection('users').find({Username:username,Password:password}).toArray();
-
-    var id = '';
-    var fn = '';
-    var ln = '';
-    var em = '';
-    var ph = '';
-
-    if( results.length > 0 )
-    {
-        id = results[0]._id.toString();
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
-        em = results[0].email;
-        ph = results[0].phone;
-    }
-
-    var ret = { id:id, firstName:fn, lastName:ln, error:''};
-    res.status(200).json(ret);
-});
-
-// register endpoint
-app.post('/api/register', async (req, res, next) => {
-    const { firstName, lastName, username, phone, email, password } = req.body;
-
-    const newUser = {FirstName: firstName, LastName: lastName, Username: username, Phone: phone, Email: email, Password: password};
-    var error = '';
-
-    try
-    {
-        const db = client.db();
-        const result = db.collection('users').insertOne(newUser);
-    }
-    catch(e)
-    {
-        error = e.toString();
-    }
-
-    var ret = { error: error };
-    res.status(200).json(ret);
-});
 
 app.use((req, res, next) => 
 {
