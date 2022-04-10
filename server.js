f0a2d9a1b171bcf07052b58b5fcb22f4cedef992
Mari Peele <mpeele@geometricprogress.com>
2022-04-09 22:35:59 -0400

    commented out stuff in server, updated package

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const sendEmail = require('./sendEmail');
+// const sendEmail = require('./sendEmail');
 
 const path = require('path');
 const PORT = process.env.PORT || 5000;
@@ -17,15 +17,15 @@ const url = process.env.MONGODB_URI;
 const url2 = process.env.MONGODB_HEALTHDB_URI;
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
-const client2 = new MONGO_CLIENT_EVENTS(url2);
+// const client2 = new MONGO_CLIENT_EVENTS(url2);
 client.connect();
-client2.connect();
+// client2.connect();
 
 let api = require('./api.js');
 let api2 = require('./habitsApi.js');
 const { MONGO_CLIENT_EVENTS } = require('mongodb');
 api.setApp( app, client);
-api2.setApp(app, client2);
+// api2.setApp(app, client2);
 
 ///////////////////////////////////////////////////
 // For Heroku deployment
