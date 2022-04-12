e7b3f5a6dff2c5c9116f80025fae45e0909b5407
unknown <ainsley.dalton2016@gmail.com>
2022-04-12 18:15:56 -0400

    send rets

--- habitsApi.js
+++ habitsApi.js
@@ -12,10 +12,14 @@ exports.setApp = function (app, client2) {
 
         const db = client2.db();
         const foundEntry = await db.collection('water').findOne({ User: username }, {Date: date});
+        let ret;
         
+        // if user already has water log for date entered, add ounces entered to existing log
         if (foundEntry){
             const id = foundEntry._id;
-            db.collection('water').updateOne({ _id: id}, { $set: { Ounces: ounces }});
+            let totalOunces = foundEntry.Ounces + ounces;
+            db.collection('water').updateOne({ _id: id}, { $set: { Ounces: totalOunces }});
+            ret = {User: username, Date: date, OuncesEntered: ounces, TotalOunces: totalOunces}
         } else {
             const waterLog = {User: username, Date: date, Ounces: ounces};
             db.collection('water').insertOne(waterLog);
@@ -36,7 +40,7 @@ exports.setApp = function (app, client2) {
         const endMeridiam = req.body.endMeridiam;
 
         const db = client2.db();
-        const foundEntry = await db.collection('sleep').findOne({User: username}, {Date: date});
+        const foundEntry = await db.collection('sleep').findOne({User: username, Date: date});
         if (foundEntry)
         {
             let ret = {error: "Sleep hours already recorded for this user and date"};
@@ -74,80 +78,98 @@ exports.setApp = function (app, client2) {
         const activity = req.body.activity;    // dropdown on front end: screen time, tv, games, sport, art, chores, work, other
 
         const db = client2.db();
-        const foundEntry = await db.collection('recreation').findOne({User: username}, {Date: date});
+        const foundEntry = await db.collection('recreation').findOne({User: username, Date: date});
+        let ret;
 
+        // if user already has an activity log for date entered, update log with new activity entered
         if (foundEntry){
             const id = foundEntry._id;
             if (activity === "Screen Time") {
                 let updatedHours = foundEntry.ScreenTime;
                 updatedHours += hours;
                 db.collection('recreation').updateOne({ _id: id }, { $set: { ScreenTime: updatedHours }});
+                ret = {User: username, Date: date, ScreenTime: updatedHours}
             }
             else if (activity === "Television") {
                 let updatedHours = foundEntry.Television;
                 updatedHours += hours;
                 db.collection('recreation').updateOne({ _id: id }, { $set: { Television: updatedHours }});
+                ret = {User: username, Date: date, Television: updatedHours}
             }
             else if (activity === "Games") {
                 let updatedHours = foundEntry.Games;
                 updatedHours += hours;
                 db.collection('recreation').updateOne({ _id: id }, { $set: { Games: updatedHours }});
+                ret = {User: username, Date: date, Games: updatedHours}
             }
             else if (activity === "Sport") {
                 let updatedHours = foundEntry.Sport;
                 updatedHours += hours;
                 db.collection('recreation').updateOne({ _id: id }, { $set: { Sport: updatedHours }});
+                ret = {User: username, Date: date, Sport: updatedHours}
             }
             else if (activity === "Art") {
                 let updatedHours = foundEntry.Art;
                 updatedHours += hours;
                 db.collection('recreation').updateOne({ _id: id }, { $set: { Art: updatedHours }});
+                ret = {User: username, Date: date, Art: updatedHours}
             }
             else if (activity === "Chores") {
                 let updatedHours = foundEntry.Chores;
                 updatedHours += hours;
                 db.collection('recreation').updateOne({ _id: id }, { $set: { Chores: updatedHours }});
+                ret = {User: username, Date: date, Chores: updatedHours}
             }
             else if (activity === "Work") {
                 let updatedHours = foundEntry.Work;
                 updatedHours += hours;
                 db.collection('recreation').updateOne({ _id: id }, { $set: { Work: updatedHours }});
+                ret = {User: username, Date: date, Work: updatedHours}
             }
             else if (activity === "Other") {
                 let updatedHours = foundEntry.Other;
                 updatedHours += hours;
                 db.collection('recreation').updateOne({ _id: id }, { $set: { Other: updatedHours }});
+                ret = {User: username, Date: date, Other: updatedHours}
             }  
         } 
         
+        // if user does not have an acitivity log for the date entered, create new activity log
         else {
             if (activity === "Screen Time") {
                 const recreationLog = {User: username, Date: date, ScreenTime: hours, Television: 0, Games: 0, Sport: 0, Art: 0, Chores: 0, Work: 0, Other: 0};
+                ret = {User: username, Date: date, ScreenTime: hours}
             }
             else if (activity === "Television") {
                 const recreationLog = {User: username, Date: date, ScreenTime: 0, Television: hours, Games: 0, Sport: 0, Art: 0, Chores: 0, Work: 0, Other: 0};
+                ret = {User: username, Date: date, Television: hours}
             }
             else if (activity === "Games") {
                 const recreationLog = {User: username, Date: date, ScreenTime: 0, Television: 0, Games: hours, Sport: 0, Art: 0, Chores: 0, Work: 0, Other: 0};
+                ret = {User: username, Date: date, Games: hours}
             }
             else if (activity === "Sport") {
                 const recreationLog = {User: username, Date: date, ScreenTime: 0, Television: 0, Games: 0, Sport: hours, Art: 0, Chores: 0, Work: 0, Other: 0};
+                ret = {User: username, Date: date, Sport: hours}
             }
             else if (activity === "Art") {
                 const recreationLog = {User: username, Date: date, ScreenTime: 0, Television: 0, Games: 0, Sport: 0, Art: hours, Chores: 0, Work: 0, Other: 0};
+                ret = {User: username, Date: date, Art: hours}
             }
             else if (activity === "Chores") {
                 const recreationLog = {User: username, Date: date, ScreenTime: 0, Television: 0, Games: 0, Sport: 0, Art: 0, Chores: hours, Work: 0, Other: 0};
+                ret = {User: username, Date: date, Chores: hours}
             }
             else if (activity === "Work") {
                 const recreationLog = {User: username, Date: date, ScreenTime: 0, Television: 0, Games: 0, Sport: 0, Art: 0, Chores: 0, Work: hours, Other: 0};
+                ret = {User: username, Date: date, Work: hours}
             }
             else if (activity === "Other") {
                 const recreationLog = {User: username, Date: date, ScreenTime: 0, Television: 0, Games: 0, Sport: 0, Art: 0, Chores: 0, Work: 0, Other: hours};
+                ret = {User: username, Date: date, Other: hours}
             }
             db.collection('recreation').insertOne(recreationLog);
         }
-
         res.status(200).json(ret);
     });
 
