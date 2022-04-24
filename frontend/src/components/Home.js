79d054c0e6deeed78f34d4f9cfbb5be8cf0f13fd
Mari Peele <mpeele@geometricprogress.com>
2022-04-23 21:03:15 -0400

    Dashboard update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -9,19 +10,24 @@ function PageTitle()
 {
    return(
        
-     <h1 id="title">
-         <Nav variant="pills" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link href="/home">Home</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="2" href="/customize">Customize</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link href="/login">Logout</Nav.Link>
-  </Nav.Item>
-</Nav>
-   HomeTemp</h1>
+    <div className = "p-5">
+      <Card className = "text-center shadow" style = {{borderRadius: 12, maxWidth: '30 rem'}}>
+        <Navbar bg="white" expland="md" style={{margin: 6}}>
+            <Navbar.Brand href="/home">
+              <img src= {require('../assets/images/dailygrind5.png')} height='30' />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me auto">
+                  <Nav.Link eventKey="1" href="/home">Home</Nav.Link>
+                  <Nav.Link href="/customize">Customize</Nav.Link>
+                  <Nav.Link href="/login">Logout</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        <Card.Text style = {{color: '#0FA3B1' , fontSize : "40px"}}>Dashboard</Card.Text> 
+      </Card>
+     </div>
    );
 };
 
