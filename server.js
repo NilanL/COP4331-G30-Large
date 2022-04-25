8f55147b96bd6f8fae341493ccfe23ef233510ae
Mari Peele <mpeele@geometricprogress.com>
2022-04-25 17:39:38 -0400

    udpated server.js

--- server.js
+++ server.js
@@ -1,149 +1,67 @@
-import React, {useState} from 'react';
-import styled, { css } from "styled-components";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css'
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
 
-function Login()
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.set('port', PORT);
+app.use(cors());
+app.use(bodyParser.json());
+
+const url = process.env.MONGODB_URI;
+const url2 = process.env.MONGODB_HEALTHDB_URI;
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url);
+//const client2 = new MONGO_CLIENT_EVENTS(url2);
+const client2 = new MongoClient(url2);
+client.connect();
+client2.connect();
+
+let api = require('./api.js');
+let api2 = require('./habitsApi.js');
+const { MONGO_CLIENT_EVENTS } = require('mongodb');
+api.setApp( app, client);
+api2.setApp(app, client2);
+
+const swaggerUi = require('swagger-ui-express'),
+  swaggerDocument = require('./swagger.json');
+
+
+///////////////////////////////////////////////////
+// For Heroku deployment
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
+  app.get('/*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
+app.use((req, res, next) => 
 {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
 
+app.use(
+  '/api-docs',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
 
-    const app_name = 'cop4331-g30-large'
-    function buildPath(route)
-    {
-        if (process.env.NODE_ENV === 'production') 
-        {
-            return 'https://' + app_name +  '.herokuapp.com/' + route;
-        }
-        else
-        {        
-            return 'http://localhost:5000/' + route;
-        }
-    }
-
-    var loginName;
-    var loginPassword;
-
-    const [message,setMessage] = useState('');
-
-
-    const doLogin = async event => 
-    {
-        event.preventDefault();
-        var obj = {username:loginName.value,password:loginPassword.value};
-        var js = JSON.stringify(obj);
-        try
-        {    
-            const response = await fetch(buildPath('api/login'),
-                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
-            var res = JSON.parse(await response.text());
-
-            setMessage('');
-
-            //console.log(js);
-            //console.log(res.id);
-            //console.log(res);
-            //console.log(res.error);
-            console.log(res.error);
-
-            if( res.error === 'Unrecognized credentials' ){
-            //if(res.code === 400){
-
-                //console.log(res.id);
-                setMessage('Username or Password is incorrect');
-            }
-            else if( res.error === 'Email is not verified can not access login' )
-            {
-                
-                setMessage('Email has not been verified');
-                //var user = {email:res.email}
-                //localStorage.setItem('user_data', JSON.stringify(user));
-                //setMessage('');
-                //window.location.href = '/notverified';
-            }
-            else
-            {
-                var user =  
-                {firstName:res.firstName,lastName:res.lastName,id:res.id}
-                localStorage.setItem('user_data', JSON.stringify(user));
-                setMessage('');
-                window.location.href = '/home';
-            }
-        }
-        catch(e)
-        {
-            alert(e.toString());
-            return;
-        }    
-    };
-
-
-    const doRegister = async event => 
-    {
-            window.location.href = '/register';
-
-    }
-
-    return(
-
-        
-      <div id="loginDiv">
-          
-  <style type="text/css">
-    {`
-    .btn-flat {
-      background-color: purple;
-      color: black;
-    }
-
-    .bg-custom-button {
-        background-color: #1F2833;
-        border-color: #45A293;
-        border: 3px solid-transparent;
-        color: #45A293;
-        border-radius: 100px;
-    }
-
-
-    .btn-xxl {
-      padding: 1rem 1.5rem;
-      font-size: 1.5rem;
-    }
-    `}
-  </style>
-
-  <Card className = "text-center" style = {{width: '50rem', height : '50rem' }}>
-        <form><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        <Card.Text style = {{color: "rgba(15, 163, 177, 100)" , fontSize : "50px"}}>Login</Card.Text> 
-        <input type="text" id="loginName" placeholder="Username" 
-            ref={(c) => loginName = c} /><br /><br />
-        <input type="password" id="loginPassword" placeholder="Password" 
-            ref={(c) => loginPassword = c} /><br /><br />
-          <Button style={{color:"#000", borderColor: '#0FA3B1', backgroundColor: "rgba(15, 163, 177, 100)"}} onClick={doLogin}>Login</Button>   <span></span> 
-          <Button style={{color:"#000", borderColor: '#0FA3B1', backgroundColor: "rgba(15, 163, 177, 100)"}} onClick={doRegister}> Register </Button> 
-          <br />
-          <a href='/RetrieveAccount'>Forgot Password</a><br />
-          <a href='/resend'>Resend Verification Email</a>
-        </form>
-        <span id="loginResult">{message}</span>
-        </Card>
-     </div>
-     
-    );
-
-
-
-};
-
-
-
-
-const Loginlogo = styled.span`
-font-family: Roboto;
-font-style: normal;
-font-weight: 700;
-color: rgba(15,163,177,1);
-font-size: 36px;
-`;
-
-export default Login;
+//app.listen(5000); // start Node + Express server on port 5000
+app.listen(PORT, () => 
+{
+  console.log('Server listening on port ' + PORT);
+});
