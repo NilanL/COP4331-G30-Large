d763501bd542cd8b9f925e75ed9cecc1fd643a4d
root <root@DESKTOP-SI0BR1S.localdomain>
2022-04-30 01:33:34 -0400

    Email Verification

---
+++ frontend/src/pages/EmailVerificationPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmailVerification from '../components/EmailVerification';
+import background from "../assets/images/backgroundLG.svg";
+
+const EmailVerificationPage = () =>
+{
+  return(
+    <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh", backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', heigh: '100%', width: '100%'}}>
+        <EmailVerification />
+      </div>
+  );
+};
+export default EmailVerificationPage;
